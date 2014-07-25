88013c215a5256c36a3790ddb6fabde323e2251f
Jack Watson <rpmeur@gmail.com>
2014-07-24 21:39:57 -0500

    yes

--- src/main/java/com/cheetojack/MagicalTechnology/reference/Reference.java
+++ src/main/java/com/cheetojack/MagicalTechnology/reference/Reference.java
@@ -9,4 +9,5 @@ public class Reference {
     public static final String VERSION = "1.7.2-0.0.1";
     public static final String CLIENT_PROXY_CLASS = "com.cheetojack.MagicalTechnology.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.cheetojack.MagicalTechnology.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.cheetojack.MagicalTechnology.client.GuiFactory";
 }
